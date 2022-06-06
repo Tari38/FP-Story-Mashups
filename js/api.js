86d5fff6c951694b2533c9217962c4297bb6af74
syedmjavaid <syedmjavaid@gmail.com>
2022-06-06 17:59:12 +0100

    Add post function

--- js/api.js
+++ js/api.js
@@ -1,51 +1,60 @@
-const userMessage = document.querySelector('#form');
-const userInput = document.querySelector('#post-input-container');
+const userMessage = document.querySelector("#form");
+const userInput = document.querySelector("#inputText");
 
 const optionsGet = {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-}
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+};
 
-const optionsPost = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        //"Accept": "application/json"
-    },
-    body: JSON.stringify({
-        id : 15,
-        time: 13.00,
-        text: 'Enter message here'
-    })
-}
+// const optionsPost = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     //"Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     id: 27,
+//     time: 13.0,
+//     text: body,
+//   }),
+// };
 
 function getPost(e) {
-     
-        e.preventDefault();
-        optionsPost.body.text = JSON.stringify(e.body)
-         fetch('https://lap1-project-backend.herokuapp.com/', optionsGet)
-        .then(resp => resp.json())
-        .then(data => document.getElementById("message").innerHTML = data.text)
-    
+  console.log(e);
+  e.preventDefault();
+
+  fetch("https://lap1-project-backend.herokuapp.com/", optionsGet)
+    .then((resp) => resp.json())
+    .then((data) => (document.getElementById("message").innerHTML = data.text));
 }
 
 function sendPost(e) {
-    
-        e.preventDefault();
-        console.log(e.body)
-         fetch('https://lap1-project-backend.herokuapp.com/', optionsPost)
-        .then(res =>{
-            return res.json()
-        })
-        .then(data => {
-            console.log(data)
-        })
-    
+  e.preventDefault();
+  let body = document.querySelector("#usertext").value;
+  console.log(body);
+  console.log(e);
+  fetch("https://lap1-project-backend.herokuapp.com/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      //"Accept": "application/json"
+    },
+    body: JSON.stringify({
+      id: 30,
+      time: 13.0,
+      text: body,
+    }),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 }
 
-
-userMessage.addEventListener('click', getPost);
-userInput.addEventListener('submit', sendPost);
+userMessage.addEventListener("click", getPost);
+userInput.addEventListener("submit", sendPost);
