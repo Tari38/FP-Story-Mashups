ce62f02b04c071330a90c5d6bf4326395d1fb6c5
Samantha Dorrell <samantha.dorrell@yahoo.co.uk>
2022-06-08 17:44:29 +0100

    added giphygit statusgit status

--- js/api.js
+++ js/api.js
@@ -1,8 +1,6 @@
-require('dotenv').config();
-
 const userMessage = document.querySelector('#form');
 const userInput = document.querySelector('#post-input-container');
-const userGif = document.querySelector('#add-gif');
+
 
 const optionsGet = {
     method: "GET",
@@ -47,29 +45,8 @@ function sendPost(e) {
             console.log(data)
         })
     
-}
-
-function addGif(gif) {
-    const response = fetch(`https://api.giphy.com/v1/gifs/search?${process.env.GIPHY_API_KEY}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: ({
-            api_key: process.env.GIPHY_API_KEY,
-            random_id: "",
-            ts: int,
-            q: "",
-            rating: "g",
-            limit: 5,
-
-        }),
-    });
-    return response.json();
-}
-        
+    }
 
 
 userMessage.addEventListener('click', getPost);
 userInput.addEventListener('submit', sendPost);
-userGif.addEventListener('click', addGif);
