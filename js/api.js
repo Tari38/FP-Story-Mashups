8f14be185e3d2f331d95a89ddb058b942e55384b
syedmjavaid <syedmjavaid@gmail.com>
2022-06-09 15:47:13 +0100

    Add gifs for post requests onto API and render them using get request

--- js/api.js
+++ js/api.js
@@ -1,84 +1,88 @@
 //require('dotenv').config();
 
+const userInput = document.querySelector("#post-input-container");
 
-
-const userInput = document.querySelector('#post-input-container');
-
-
-const userGif = document.querySelector('#add-gif');
+const userGif = document.querySelector("#add-gif");
 
 const optionsGet = {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-}
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+};
 
- 
-   
-///// send userMessage to the server and renders the message on the webpage //////                    
+///// send userMessage to the server and renders the message on the webpage //////
 
 function sendPost(e) {
+  e.preventDefault();
+  const textMessage =
+    document.getElementsByClassName("textArea")[0].childNodes[0].textContent;
 
-    e.preventDefault();
-    const textMessage = document.getElementsByClassName("textArea")[0].childNodes[0].textContent
-    // const textMessage = e.target.textInput.value;
-    const randomId = Math.floor(Math.random() * 4000);
-    console.log(randomId);
+  const giphyImg = document.getElementById("giphyId").src;
 
-    // let randomId;
-    // const idFetch = fetch('https://lap1-project-backend.herokuapp.com/all' , optionsGet)
-    //     .then(res=> res.json())
-    //     .then(data => {
-    //         randomId = data.length + 1
-    //     })
-    //     .catch(err => console.log(err))
-    
-    // console.log(randomId)
-   
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            //"Accept": "application/json"
-        },
-        body: JSON.stringify({
-            id : randomId,
-            time: new Date().toLocaleString(),
-            text: textMessage
-        })
-    }
-    
-    userInput.reset();
-    
-    const print = fetch('https://lap1-project-backend.herokuapp.com/post', options)
-    .then(res => res.json())
-    .then(data => {
-        renderMsg()
-        console.log(data)
+  // const textMessage = e.target.textInput.value;
+  const randomId = Math.floor(Math.random() * 4000);
+  console.log(randomId);
+
+  // let randomId;
+  // const idFetch = fetch('https://lap1-project-backend.herokuapp.com/all' , optionsGet)
+  //     .then(res=> res.json())
+  //     .then(data => {
+  //         randomId = data.length + 1
+  //     })
+  //     .catch(err => console.log(err))
+
+  // console.log(randomId)
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      //"Accept": "application/json"
+    },
+    body: JSON.stringify({
+      id: randomId,
+      time: new Date().toLocaleString(),
+      text: textMessage,
+      gif: giphyImg,
+    }),
+  };
+
+  userInput.reset();
+
+  const print = fetch(
+    "https://lap1-project-backend.herokuapp.com/post",
+    options
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      renderMsg();
+      console.log(data);
     })
-    .catch(err => console.log(err))
-
-    
-    
-    
-    
+    .catch((err) => console.log(err));
 }
 
-function renderMsg(){
+function renderMsg() {
+  const test = fetch(
+    `https://lap1-project-backend.herokuapp.com/all`,
+    optionsGet
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      const container = document.getElementById("submitted-post-div");
 
-const test = fetch(`https://lap1-project-backend.herokuapp.com/all`, optionsGet)
-    .then(resp => resp.json())
-    .then(data => {
-        const container = document.getElementById('submitted-post-div');
-           
-    const reverseData = data.reverse()
-        const returnCards =  (reverseData) => {
-            return "<div class=\"products-cards\">" + data.map(valueCards => `
+      const reverseData = data.reverse();
+      const returnCards = (reverseData) => {
+        return (
+          '<div class="products-cards">' +
+          data
+            .map(
+              (valueCards) => `
             <div>
                 <div class="product-headerzz">
                 <div class="product-header"><p>${valueCards.text}</p></div>
+                <div class="product-image"><img src="${valueCards.gif}" alt='no gif'></div>
                 <div class="btn-group shadow" role="group">
                     <input type="checkbox" id="r1" class="btn">
                         <label for="r1" class="react">
@@ -104,61 +108,54 @@ const test = fetch(`https://lap1-project-backend.herokuapp.com/all`, optionsGet)
                 </div>
                 </div>
 
-            </div>`).join('') + "</div>";
-                
-        }
+            </div>`
+            )
+            .join("") +
+          "</div>"
+        );
+      };
 
-        //console.log(returnCards(data))
-        container.innerHTML = returnCards(data)
+      //console.log(returnCards(data))
+      container.innerHTML = returnCards(data);
     })
 
-    .catch(err => console.log(err))
-
+    .catch((err) => console.log(err));
 }
 
-renderMsg()
-userInput.addEventListener('submit', sendPost);
+renderMsg();
+userInput.addEventListener("submit", sendPost);
 
 function addGif(gif) {
-    const response = fetch(`https://api.giphy.com/v1/gifs/search?${process.env.GIPHY_API_KEY}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: ({
-            api_key: process.env.GIPHY_API_KEY,
-            random_id: "",
-            ts: integer,
-            q: "",
-            rating: "g",
-            limit: 5,
-
-        }),
-    });
-    return response.json();
+  const response = fetch(
+    `https://api.giphy.com/v1/gifs/search?${process.env.GIPHY_API_KEY}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        api_key: process.env.GIPHY_API_KEY,
+        random_id: "",
+        ts: integer,
+        q: "",
+        rating: "g",
+        limit: 5,
+      },
+    }
+  );
+  return response.json();
 }
-        
-
-
-
-
-
-
-
-
-
 
 //userMessage.addEventListener('click', getPost);
 //userInput.addEventListener('onClick', sendPost);
 
-userGif.addEventListener('click', addGif);
-
+userGif.addEventListener("click", addGif);
 
 // function onButtonClick (e) {
 //     e.preventDefault();
 //     console.log("words test")
 //     document.getElementById('reply-to-post').className = "show";
-    
+
 // }
 
 // const clickComment = document.querySelector("#reply-to-post")
--- js/index.js
+++ js/index.js
@@ -6,45 +6,44 @@
 // require("dotenv").config();
 
 function init() {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  };
 
-    const options = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
-    }  
-        
-        document.getElementById('gifSearch').addEventListener('submit', e => {
-            e.preventDefault();
-    
-        let str = document.getElementById("searchGif").value.trim();
-        let url = `https://api.giphy.com/v1/gifs/search?api_key=FlLcKnBPn6Wpd1tyqGXPsXj8WEbOhCYQ&q=${str}&limit=1&offset=0&rating=g&lang=en`;
-        // url = url.concat(str);
-        console.log(url);
-        fetch(url)
-            .then(resp => resp.json())
-            .then(content => {
-                console.log(content);
-                // data, pagination, meta
-                // console.log(content.data.url);
-                console.log("META", content.meta);
-                let fig = document.createElement("figure");
-                let img = document.createElement("img");
-                let fc = document.createElement("figcaption");
-                let textInput = document.querySelector("#textInput");
-                img.src = content.data[0].images.downsized.url;
-                img.alt = content.data.title;
-                fc.textContent = content.data.title;
-                fig.appendChild(img);
-                fig.appendChild(fc);
-                textInput.insertAdjacentElement("afterbegin", fig);
-                document.querySelector("#searchGif").value = '';
+  document.getElementById("gifSearch").addEventListener("submit", (e) => {
+    e.preventDefault();
 
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    });
-    };
-    document.addEventListener("DOMContentLoaded", init);
+    let str = document.getElementById("searchGif").value.trim();
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=FlLcKnBPn6Wpd1tyqGXPsXj8WEbOhCYQ&q=${str}&limit=1&offset=0&rating=g&lang=en`;
+    // url = url.concat(str);
+    console.log(url);
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((content) => {
+        console.log(content);
+        // data, pagination, meta
+        // console.log(content.data.url);
+        console.log("META", content.meta);
+        let fig = document.createElement("figure");
+        let img = document.createElement("img");
+        let fc = document.createElement("figcaption");
+        let textInput = document.querySelector("#textInput");
+        img.src = content.data[0].images.downsized.url;
+        img.alt = content.data.title;
+        img.id = "giphyId";
+        fc.textContent = content.data.title;
+        fig.appendChild(img);
+        fig.appendChild(fc);
+        textInput.insertAdjacentElement("afterbegin", fig);
+        document.querySelector("#searchGif").value = "";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
+document.addEventListener("DOMContentLoaded", init);
